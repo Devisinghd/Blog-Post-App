b5f14138188dc59cb6ee564c3bec3c92e8e1e40c
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-25 17:42:56 +0530

    appname modifies

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blogapp'
+    'blogapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
