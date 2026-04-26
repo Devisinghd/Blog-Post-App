ce10d32a5985dc7babc2d7ea6f325ec2bd8dd1da
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-26 16:31:12 +0530

    created add-blog view

--- myblog/blogapp/views.py
+++ myblog/blogapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .models import blog
+from .form import blogform
 # Create your views here.
+
+def index(request):
+    blogs = blog.objects.all()
+    return render(request,'blogapp/index.html',{'blogs':blogs})
+
+def detail(request,id):
+    post = blog.objects.get(id=id)
+    return render(request,'blogapp/detail.html',{'post':post})
+
+def create(request):
+    form = blogform(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('blogapp:index')
+    else:
+        form = blogform(request.POST or None)
+    return render(request,'blogapp/add-blog.html',{'form':form})
+    
