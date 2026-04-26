5cc8d6ae757eb7fdf5a89b61506b9c225fa57977
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-26 16:29:43 +0530

    chenged slugfield

--- myblog/blogapp/models.py
+++ myblog/blogapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here
+class blog(models.Model):
+
+
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True)
+    summary = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True,blank=True)
+    updated_at = models.DateTimeField(auto_now_add=True,blank=True)
+    published_date = models.DateTimeField()
+    is_published = models.BooleanField()
