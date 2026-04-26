1c9f50a370c496206ad9cb6d96cdbaca75e71ce7
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-26 16:31:38 +0530

    created add-blog url pattern

--- myblog/blogapp/urls.py
+++ myblog/blogapp/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
+
+app_name = 'blogapp'
 
 urlpatterns = [
-    
+    path('',views.index,name='index'),
+    path('<int:id>',views.detail,name='detail'),
+    path('create',views.create,name='create'),
+    path('update/<int:id>',views.update,name='update'),
+    path('delete/<int:id>',views.delete,name='delete'),
 ]
