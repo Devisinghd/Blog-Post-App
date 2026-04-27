5756d3f4ee06df902dbe2eb380f81bf051c47ea2
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-27 16:50:47 +0530

    update and delete url

--- myblog/blogapp/urls.py
+++ myblog/blogapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create',views.create,name='create'),
     path('update/<int:id>',views.update,name='update'),
     path('delete/<int:id>',views.delete,name='delete'),
+    path('toggle/<int:id>',views.toggle_published,name='toggle_published'),
 ]
