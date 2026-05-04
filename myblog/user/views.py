e1890940888c87ac5cd19b11e8565a717211c0e3
devisingh dangi <devisinghdangi786@gmail.com>
2026-05-04 20:13:57 +0530

    views created for login,logout

--- myblog/user/views.py
+++ myblog/user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .form import Register
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = Register(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user:login')
+    else:
+        form = Register()
+    return render(request,'user/register.html',{'form':form})
+
+def logout_view(request):
+    logout_view(request)
+    return render(request,'user/logout.html')
